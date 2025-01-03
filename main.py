9eec343b835dce1f80ab9dcbfeb4feaa15707368
Steven Mings <stevennmings@gmail.com>
2025-01-03 14:14:45 -0600

    Added the Delta Time to the game loop

--- main.py
+++ main.py
@@ -8,25 +8,19 @@ def main():
     #Set the screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
-
-
-    print("Attempting to access constants...")
-    print("Screen Width:", SCREEN_WIDTH)
-    print("Screen Height:", SCREEN_HEIGHT)
-    #Print Statements
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
 
     n = False
     while n == False:
+        dt = clock.tick(60) / 1000
         screen.fill((0,0,0))
         pygame.display.flip()
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+    
 
 
 if __name__ == "__main__":
